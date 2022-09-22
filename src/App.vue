78a59a92b211ca2c48adc5e10de56c6a7ba4a7b9
rodrigo2409 <108556826+rodrigo2409@users.noreply.github.com>
2022-09-22 12:22:04 -0500

    examenAWS

---
+++ src/App.vue
@@ -0,0 +1,211 @@
+<template>
+  <header>
+    <nav>
+        <ul>
+		<li><a href="#">hamburgesa</a></li>
+		<li><a href="#">papas</a></li>
+		<li><a href="#">pizza</a></li>
+		<li><a href="#">burritos</a></li>
+		<li><a href="#">tacos</a></li>
+    <li><a href="#">bebidas</a></li>
+    <li><a href="#">postres</a></li>
+	 </ul>
+    </nav>
+</header>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div>
+<h1>comida rapida Rodrigo Eleazar Olea Alvarado 4A</h1> 
+</div>
+
+
+
+<body>
+    <div class="container">
+        
+
+        <div class="card">
+            <img url="https://img.freepik.com/psd-premium/hamburguesa-fresca-sabrosa-fondo-transparente_670625-655.jpg?w=2000">
+            <h4>hamburgesa</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $40</p>
+            <a href="#">Leer más</a>
+        </div>
+    
+
+        <div class="card">
+            <img url="https://www.pngwing.com/es/search?q=burrito">
+            <h4>burritos</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: 40$</p>
+            <a href="#">Leer más</a>
+        </div>
+
+        <div class="card">
+            <img url="https://img2.freepng.es/20171216/60e/fries-png-5a34b8b484dc53.5973590915134045965442.jpg">
+            <h4>papas fritas</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $40</p>
+            <a href="#">Leer más</a>
+        </div>
+        <div class="card">
+            <img url="https://i.pinimg.com/originals/60/7a/9b/607a9bc3f86378637f1e8c5e56cd51c8.jpg">
+            <h4>piza</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $140</p>
+            <a href="#">Leer más</a>
+        </div>
+        <div class="card">
+            <img url="https://www.pngfind.com/pngs/m/300-3002185_tacos-de-asada-png-greek-salad-transparent-png.png">
+            <h4>tacos</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $15</p>
+            <a href="#">Leer más</a>
+        </div>
+        <div class="card">
+            <img url="https://img2.freepng.es/20180409/hqe/kisspng-fizzy-drinks-energy-drink-pepsi-fast-food-drink-5acaf916bf2816.082727551523251478783.jpg">
+            <h4>bebidas</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $20</p>
+            <a href="#">Leer más</a>
+        </div>
+        <div class="card">
+            <img url="https://img2.freepng.es/20181211/icy/kisspng-cupcake-donuts-frosting-icing-dessert-menu-5c1021886372c2.8954849915445610324074.jpg">
+            <h4>postres</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <p>costo: $30</p>
+            <a href="#">Leer más</a>
+        </div>
+
+    </div>
+
+</body>
+
+</template>
+<style>
+    header {
+        background: rgba(0, 0, 0, 0.7);
+        width: 100%;
+        position: fixed;
+        z-index: 100;
+    }
+    
+    nav {
+        float: left;
+    }
+    
+    nav ul {
+        list-style: none;
+        overflow: hidden;
+    }
+    
+    nav ul li {
+        float: left;
+        font-family: Arial, Helvetica, sans-serif;
+        ;
+        font-size: 20px;
+    }
+    
+    nav ul li a {
+        display: block;
+        padding: 20px;
+        color: #fff;
+        text-decoration: none;
+    }
+    
+    nav ul li:hover {
+        background: #eca023;
+    }
+    
+    text-decoration none nav ul li:hover {
+        background: #eca023;
+    }
+    
+    text-decoration none nav ul li:hover {
+        background: #eca023;
+    }
+    
+    text-decoration none nav ul li:hover {
+        background: #eca023;
+    }
+</style>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    
+    body {
+        width: 100%;
+        height: 100%;
+        font-family: sans-serif;
+        letter-spacing: 0.03em;
+        line-height: 1.6;
+        font-family: 'Open Sans', sans-serif;
+    }
+    
+    .title {
+        text-align: center;
+        font-size: 40px;
+        color: #6a6a6a;
+        margin-top: 100px;
+        font-weight: 100;
+        font-family: 'Roboto', sans-serif;
+    }
+    
+    .container {
+        width: 100%;
+        max-width: 1200px;
+        height: 430px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin: auto;
+    }
+    
+    .container .card {
+        width: 330px;
+        height: 430px;
+        border-radius: 8px;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+        overflow: hidden;
+        margin: 20px;
+        text-align: center;
+        transition: all 0.25s;
+    }
+    
+    .container .card:hover {
+        transform: translateY(-15px);
+        box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+    }
+    
+    .container .card img {
+        width: 330px;
+        height: 220px;
+    }
+    
+    .container .card h4 {
+        font-weight: 600;
+    }
+    
+    .container .card p {
+        padding: 0 1rem;
+        font-size: 16px;
+        font-weight: 300;
+    }
+    
+    .container .card a {
+        font-weight: 500;
+        text-decoration: none;
+        color: #3498db;
+    }
+</style>
+<style>
+    body {
+        background-color: linen;
+    }
+</style>
